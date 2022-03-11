2ad4311084e28b565114b03e8bb4d7ac6e30f965
Hanafi-tech <hanzcreative1@gmail.com>
2022-03-11 10:31:25 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      height: {
+        md: '30rem',
+        lg: '40rem',
+        xl: '60rem'
+      }
+    },
+  },
+  plugins: [],
+}
